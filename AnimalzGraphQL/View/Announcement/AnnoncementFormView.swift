3fb5a0d1d0f0997f457e41509c86695f74386315
Cindy Nguyen <cindynguyn@gmail.com>
2021-09-21 17:30:01 +0200

    [Annoncement] : Done

--- AnimalzGraphQL/View/Announcement/AnnoncementFormView.swift
+++ AnimalzGraphQL/View/Announcement/AnnoncementFormView.swift
@@ -8,35 +8,56 @@
 import SwiftUI
 
 struct AnnoncementFormView: View {
-    var arrayOfNames = ["Bobby", "Pilou", "Pépé", "Ora"]
-    var arrayOfTypes = ["Rencontre amicale", "Copuler", "Vente"]
-    @State private var selectedTypes = 0
-    @State private var selectedIndex = 0
+    @Environment(\.presentationMode) private var presentationMode
+    @State private var selectedTypes: AnnoucementType = .vente
+    @State private var selectedName = 0
     @State private var message = ""
+    @State private var title = ""
     @State private var textStyle = UIFont.TextStyle.body
+    @ObservedObject private var appSetting = AppSetting.shared
     
+    func getArrayOfNames() -> [String] {
+        var names: [String] = []
+       appSetting.userFields?.animalList?.forEach({ animal in
+            names.append(animal?.fragments.allAnimalFields.name ?? "")
+        })
+        return names
+    }
     
+    func getId(index: Int) -> String {
+        var animals: [String] = []
+        appSetting.userFields?.animalList?.forEach({ animal in
+            guard let animal = animal else {
+                return
+            }
+            animals.append(animal.fragments.allAnimalFields.animalId ?? "")
+        })
+        return animals[index]
+    }
     var body: some View {
         VStack {
             Text("Nouvelle annonce")
                 .font(.title)
                 .foregroundColor(.orange)
             Text("Nom de l'animal")
-            Picker("Names", selection: $selectedIndex) {
-                ForEach(0 ..< arrayOfNames.count) {
-                    Text(self.arrayOfNames[$0])
+            Picker("Names", selection: $selectedName) {
+                ForEach(0 ..< getArrayOfNames().count) {
+                    Text(self.getArrayOfNames()[$0])
                 }
             }
             .pickerStyle(SegmentedPickerStyle())
             
             Text("Type d'annonce")
             Picker("Types", selection: $selectedTypes) {
-                ForEach(0 ..< arrayOfTypes.count) {
-                    Text(self.arrayOfTypes[$0])
+                ForEach(AnnoucementType.allCases, id: \.self) { value in
+                    Text(value.rawValue).tag(value)
                 }
             }
             .pickerStyle(SegmentedPickerStyle())
             
+            Text("Titre de l'annonce")
+            TextField("Titre", text: $title)
+            Text("Description de l'annonce")
             TextEditor(text: $message)
                 .padding()
                 .overlay(RoundedRectangle(cornerRadius: 25)
@@ -46,11 +67,12 @@ struct AnnoncementFormView: View {
             Button(action: {
                 // REQUETE ANNONCE
                 
+                self.presentationMode.wrappedValue.dismiss()
+                Network.shared.apollo.perform(mutation: NewAnnoucementMutationWithVariablesMutation(annoucement: newAnnoucementInput(animalId: getId(index: self.selectedName), title: self.title, description: self.message, announcementType: self.selectedTypes)))
+                
             }, label: {
                 Text("Valider")
             })
-            
-            Spacer()
         }
         
     }
